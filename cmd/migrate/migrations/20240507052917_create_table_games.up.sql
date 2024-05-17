44398dc3597df70462032e433ded2f1b3b89c9cd
Mike Murphy <michael.w.muprhy@outlook.com>
2024-05-17 06:17:54 +0100

    Adds additional columns to the games table

--- cmd/migrate/migrations/20240507052917_create_table_games.up.sql
+++ cmd/migrate/migrations/20240507052917_create_table_games.up.sql
@@ -1,9 +1,13 @@
 create table if not exists games (
     id integer primary key,
+    name text not null,
     number text not null,
     player_uid integer not null,
     api_key text not null,
     start_time integer not null,
     tick_rate integer not null,
-    production_rate integer not null
+    production_rate integer not null,
+    started integer not null check(started in (0, 1)),
+    paused integer not null check(paused in (0, 1)),
+    game_over integer not null check(game_over in (0, 1))
 );
