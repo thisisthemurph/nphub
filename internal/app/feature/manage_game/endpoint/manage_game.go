d26af13305c68c28b91ed7accf43c63f8d9f07aa
Mike Murphy <michael.w.muprhy@outlook.com>
2024-05-18 16:24:59 +0100

    Adds user data section

--- internal/app/feature/manage_game/endpoint/manage_game.go
+++ internal/app/feature/manage_game/endpoint/manage_game.go
@@ -26,6 +26,7 @@ func NewManageGameEndpoint(gameRouteParams *params.GameRouteParams) contract.End
 
 func (ep *manageGameEndpoint) MapEndpoint() {
 	ep.GameGroup.GET("/:gameId", ep.handler())
+	ep.GameGroup.GET("/:gameId/:playerUid", ep.playerDataHandler())
 }
 
 func (ep *manageGameEndpoint) handler() echo.HandlerFunc {
@@ -55,5 +56,43 @@ func (ep *manageGameEndpoint) handler() echo.HandlerFunc {
 		}
 
 		return ui.Render(c, view.ManageGamePage(viewBag))
+	}
+}
+
+func (ep *manageGameEndpoint) playerDataHandler() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		gameIdParam := c.Param("gameId")
+		gameId, err := strconv.Atoi(gameIdParam)
+		if err != nil {
+			return err
+		}
+
+		playerUIDParam := c.Param("playerUid")
+		playerUID, err := strconv.Atoi(playerUIDParam)
+		if err != nil {
+			return err
+		}
+
+		grCmd := command.NewGetGameByRowIDQuery(gameId)
+		gameResult, err := mediatr.Send[*command.GetGameByRowIDQuery, model.Game](c.Request().Context(), grCmd)
+		if err != nil {
+			return err
+		}
+
+		ssCmd := command.NewGetLatestScanningDataQuery(gameResult.Number, gameResult.PlayerUID)
+		snapshotResult, err := mediatr.Send[*command.GetLatestScanningDataQuery, npmodel.ScanningData](c.Request().Context(), ssCmd)
+		if err != nil {
+			return err
+		}
+
+		player, _ := snapshotResult.Players.Get(playerUID)
+		homeStar, _ := snapshotResult.Stars.Get(player.HomeStarUID)
+
+		bag := view.PlayerDataViewBag{
+			HomeStar: homeStar,
+			Player:   player,
+		}
+
+		return ui.Render(c, view.PlayerData(bag))
 	}
 }
