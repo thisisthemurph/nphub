f10ed8e476e57de90c6d11820839cad39981e8ef
Mike Murphy <michael.w.muprhy@outlook.com>
2024-05-19 16:02:15 +0100

    Validates the new game form

--- internal/app/feature/create_game/endpoint/create_game.go
+++ internal/app/feature/create_game/endpoint/create_game.go
@@ -1,10 +1,15 @@
 package endpoint
 
 import (
+	"errors"
 	"fmt"
+	"nphud/internal/app/feature/create_game/command"
+	"nphud/internal/app/feature/create_game/view"
+	"nphud/internal/app/shared/ui"
+	"strings"
+
 	"github.com/labstack/echo/v4"
 	"github.com/mehdihadeli/go-mediatr"
-	"nphud/internal/app/feature/create_game/command"
 )
 
 type CreateGameRequest struct {
@@ -12,17 +17,30 @@ type CreateGameRequest struct {
 	APIKey     string `json:"api_key" form:"api_key"`
 }
 
+var (
+	ErrGameNumberRequired = errors.New("the game number is required")
+	ErrGameAPIKeyRequired = errors.New("the API key is required")
+)
+
 func (ep *createGameEndpoint) createGameHandler() echo.HandlerFunc {
 	return func(c echo.Context) error {
+		ctx := c.Request().Context()
 		request := &CreateGameRequest{}
 		if err := c.Bind(request); err != nil {
 			return err
 		}
 
-		// TODO: Validate the request
+		form := view.NewCreateGameFormProps(
+			strings.TrimSpace(request.GameNumber),
+			strings.TrimSpace(request.APIKey),
+		)
+
+		if !form.Validate() {
+			return ui.Render(c, view.CreateGameForm(form))
+		}
 
 		cmd := command.NewCreateGameCommand(request.GameNumber, request.APIKey)
-		result, err := mediatr.Send[*command.CreateGameCommand, command.CreateGameResult](c.Request().Context(), cmd)
+		result, err := mediatr.Send[*command.CreateGameCommand, command.CreateGameResult](ctx, cmd)
 		if err != nil {
 			return err
 		}
