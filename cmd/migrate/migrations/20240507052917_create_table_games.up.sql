5db4edfec2d5192c2d06fc2ce377fc5dd5847cc4
Mike Murphy <michael.w.muprhy@outlook.com>
2024-05-20 05:48:22 +0100

    Adds next_snapshot_at column to games table to better track when the next snapshot should be taken

--- cmd/migrate/migrations/20240507052917_create_table_games.up.sql
+++ cmd/migrate/migrations/20240507052917_create_table_games.up.sql
@@ -9,5 +9,6 @@ create table if not exists games (
     production_rate integer not null,
     started integer not null check(started in (0, 1)),
     paused integer not null check(paused in (0, 1)),
-    game_over integer not null check(game_over in (0, 1))
+    game_over integer not null check(game_over in (0, 1)),
+    next_snapshot_at integer
 );
